65e5c654c62eefc0c40085f15539056a32b18d4d
gaikiir <gaichris380@gmail.com>
2025-03-25 12:25:50 +0300

    Implement new methods to the route

--- routes/StudentRoute.js
+++ routes/StudentRoute.js
@@ -1,24 +1,37 @@
 const express = require("express");
-const router = express.Router();
+const route = express.Router();
 
 const student_Route = require("../StudentController/studentController");
 
 // authenticate middleware
 const {verifyAccessToken } = require('../helpers/JwtHelper');
+const restrctUser = require('../helpers/JwtHelper')
 
 //get all students from the database
-router.get("/getAllStudents", verifyAccessToken, student_Route.getAllStudents);
+route.get("/getAllStudents", student_Route.getAllStudents);
 
-//add a student to the database 
-router.post("/addStudent", student_Route.addStudent);
+
+//add a student to the database
+//verifyAccessToken, restrctUser.restrict('user', 'admin') 
+route.post("/Addstudent",student_Route.Addstudent);
 
 //get a student by id
-router.get("/getStudentById/:id", student_Route.getStudentById);
+//verifyAccessToken, restrctUser.restrict('user', 'admin'), 
+route.get("/getStudent/:id", student_Route.getStudent);
+
 
 //update a student by id
-router.put("/updateStudent/:id", student_Route.updateStudent);
+
+//verifyAccessToken, restrctUser.restrict('user', 'admin'), 
+route.put("/updateStudent/:id", student_Route.updateStudent);
+
+route.patch("/updateStudent/:id", student_Route.updateStudent);
+
 
 //delete a student by id
-router.delete('/deleteStudent/:id', student_Route.deleteStudent);
+//verifyAccessToken, restrctUser.restrict('user', 'admin'),
+route.delete('/deleteStudent/:id', student_Route.deleteStudent);
 
-module.exports = router;
+
+module.exports = route;
+
